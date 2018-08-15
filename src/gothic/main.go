27bfcfbaf98e326b57f4bbb63e83709a5cd51b16
Labiak Taras kissarat@gmail.com <kissarat@gmail.com>
2018-08-15 21:05:03 +0300

    + github.com/gorilla/mux

--- src/gothic/main.go
+++ src/gothic/main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello from Go!")
+	vars := mux.Vars(r)
+	fmt.Fprintln(w, "Hello from "+vars["number"])
 }
 
 // Hello HTTP Server
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+	r.HandleFunc("/hello/{number}", handler)
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
